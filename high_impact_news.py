29afbd393f589979b338be3c452cad7e2ed8033a
Olumide Ajilore <43110012+AlaafinOlumide@users.noreply.github.com>
2025-12-12 12:53:03 +0000

    Update high_impact_news.py

--- high_impact_news.py
+++ high_impact_news.py
@@ -131,3 +131,6 @@ def has_high_impact_news_near(symbol: str, now_utc: dt.datetime, window_minutes:
             return True
 
     return False
+    def has_high_impact_news_nearby(now_utc):
+    return has_high_impact_news_near(now_utc)
+
